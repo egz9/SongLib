ec0f03a4b9a06e7803211fb143de42f5f9043f4d
ag1465 <33241158+ag1465@users.noreply.github.com>
2018-09-28 23:07:20 -0400

    Song Class with instance and compare

--- src/application/Song.java
+++ src/application/Song.java
@@ -1,5 +1,33 @@
 package application;
 
 public class Song {
-
+	//Instance Variables
+	String name;
+	String artist;
+	String album;
+	int year;
+    		
+	public Song(String name, String artist, String album, int year)
+	{
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+		this.year = year;
+	}
+	public static boolean IsSame(Song s, Song s2)
+	{
+		if(s.name == s2.name)
+		
+			if(s.artist == s2.artist)
+				{
+				return true;
+				}
+			else
+			{
+				add()
+			}
+		}
+		return false;
+		
+	}
 }
