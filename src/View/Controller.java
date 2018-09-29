32e76c28c1e9fb812ac264d25d6694cccdd7ce7e
Eric Zimmerman <32339982+egz9@users.noreply.github.com>
2018-09-29 19:24:06 -0400

    Added cancel button
    cancel button now pops up when u click add or edit. also added more helper methods and comments to controller.java

--- src/View/Controller.java
+++ src/View/Controller.java
@@ -11,12 +11,13 @@ import java.util.ResourceBundle;
 
 public class Controller {
 	
-	@FXML ListView listArea;
+	@FXML ListView<String> listArea;
 	
+	@FXML Button addB;
 	@FXML Button editB;
 	@FXML Button delB;
 	@FXML Button saveB;
-	@FXML Button addB;
+	@FXML Button cnclB;
 	
 	@FXML TextField songField;
 	@FXML TextField artistField;
@@ -28,11 +29,7 @@ public class Controller {
 	 */
 	public void addButton(ActionEvent e){
 		//disable input anywhere but detail display
-		listArea.setDisable(true);
-		addB.setDisable(true);
-		editB.setDisable(true);
-		delB.setDisable(true);
-		
+		disableAllButDD(true);
 		
 		//clear text in each box, so the user knows they can change it
 		songField.clear();
@@ -43,55 +40,67 @@ public class Controller {
 		//set each detail as editable so user can add new info
 		allowDetailEdits(true);
 		
-		//save button pops up, to let program know when user wants to save changes
+		//save button pops up. It will be the only button besides cancel
+		//that the user can push
 		saveB.setVisible(true);
-		/*
-		saveB.setOnAction(event -> {
-			System.out.println("dog");
-			System.out.println(songField.getText() + "_" + artistField.getText() + "_"
-			+ albumField.getText() + "_" + yearField.getText());
-			listArea.setDisable(false);
-			editB.setDisable(false);
-			delB.setDisable(false);
-			addB.setDisable(false);
-			allowDetailEdits(false);
-			saveB.setVisible(false);
-			return;
-		});
-		*/
+		cnclB.setVisible(true);
 	}
+	
 	/* allows text fields to be changed. once user saves changes, the fields
 	 * will no longer allow changes
 	 */
 	public void editButton(ActionEvent e){
-		//disable input everywhere but detail display
-		listArea.setDisable(true);
-		addB.setDisable(true);
-		editB.setDisable(true);
-		delB.setDisable(true);
 		
-		allowDetailEdits(true);
+		disableAllButDD(true); 	//disable input everywhere but detail display
+		allowDetailEdits(true); //set each detail as editable so user can add new info
 		saveB.setVisible(true);
+		cnclB.setVisible(true);
 	}
 	
-	//add to fxml
 	public void saveButton(){
+		//print some info to ensure save button is working correctly
 		System.out.println("dog");
 		System.out.println(songField.getText() + "_" + artistField.getText() + "_"
-		+ albumField.getText() + "_" + yearField.getText());
-		listArea.setDisable(false);
-		editB.setDisable(false);
-		delB.setDisable(false);
-		addB.setDisable(false);
+				+ albumField.getText() + "_" + yearField.getText());
+		
+		disableAllButDD(false);
 		allowDetailEdits(false);
 		saveB.setVisible(false);
+		cnclB.setVisible(false);
 	}
 	
-	public void allowDetailEdits(boolean b){
+	public void cancelButton(){
+		
+		disableAllButDD(false); 
+		allowDetailEdits(false);
+		saveB.setVisible(false);
+		cnclB.setVisible(false);
+		
+		//not sure if I'm going to keep this
+		songField.clear();
+		artistField.clear();
+		albumField.clear();
+		yearField.clear();
+	}
+	
+	
+	//if b = true: song, artist, album and year text fields can
+	//be edited by user. if b = false: the text fields can not be
+	//edited by user
+	private void allowDetailEdits(boolean b){
 		songField.setEditable(b);
 		artistField.setEditable(b);
 		albumField.setEditable(b);
 		yearField.setEditable(b);
+	}
+	
+	//if b = true: disable input everywhere but detail display
+	//if b = false: re-enable input everywhere
+	private void disableAllButDD(boolean b){
+		listArea.setDisable(b);
+		addB.setDisable(b);
+		editB.setDisable(b);
+		delB.setDisable(b);
 	}
 	
 }
