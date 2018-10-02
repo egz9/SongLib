838428a247cca7823bb258edd0a7898400788266
ag1465 <33241158+ag1465@users.noreply.github.com>
2018-10-02 01:50:27 -0400

    edit

--- src/View/Controller.java
+++ src/View/Controller.java
@@ -165,18 +165,17 @@ public class Controller {
 		{
 			
 			Song selectedSong = listArea.getSelectionModel().getSelectedItem();
-			try {
-				SongLib.delFromSongList((ObservableList<Song>)listArea.getItems(), selectedSong);
-			} catch (IOException e) {
-			// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
 			
 			Song s2 = new Song(songField.getText(), artistField.getText(), albumField.getText(), yearField.getText());
 			if(SongLib.add2SongList(songList, s2))
 			{
 				SongLib.add2SongList(songList, s2);
+				try {
+					SongLib.delFromSongList((ObservableList<Song>)listArea.getItems(), selectedSong);
+				} catch (IOException e) {
+				// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				edit = 0;
 			}
 			else
